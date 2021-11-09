1d7796a256911b8ebc5b379733216743c7e47871
Vincent <adamvincent163@gmail.com>
2021-11-08 22:43:03 -0400

    Update query

--- Database.js
+++ Database.js
@@ -24,7 +24,7 @@ module.exports = class Database {
     }
 
     async getAutomations() {
-        const res = await this.client.query('SELECT * FROM automations')
+        const res = await this.client.query('SELECT * FROM automations ORDER BY id asc')
         return res.rows
     }
 
